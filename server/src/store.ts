c1a72ac01a488c206714b17670b348eb2e3d92b2
gosho-kazuya <ketsume0211@gmail.com>
2020-04-14 00:15:28 +0900

    remove comment

--- server/src/store.ts
+++ server/src/store.ts
@@ -2,28 +2,6 @@ import { createStore } from 'redux'
 import { PokerAction } from '@fastpoker/core'
 import { Repository } from './repositories'
 
-// function addCardIfBetAdjusted(room: Room): Room {
-//   const maxBetAmount = getCurrentMaximumBet(room)
-//   if (
-//     (maxBetAmount > 0 &&
-//       room.players
-//         .filter(player => player.isActive)
-//         .every(player => player.betting === maxBetAmount)) ||
-//     (maxBetAmount === 0 &&
-//       room.players.filter(player => player.isActive).every(player => player.checed))
-//   ) {
-//     room.players.forEach(player => {
-//       player.betting = 0
-//     })
-//     if (room.board.cards.length === 0) {
-//       room.board.cards = [createCard(), createCard(), createCard()]
-//     } else {
-//       room.board.cards.push(createCard())
-//     }
-//   }
-//   return room
-// }
-
 function reducer(repository: Repository = new Repository(), action: PokerAction): Repository {
   if (!action || !action.payload) {
     return repository
