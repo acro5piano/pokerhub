055c217830023490756d05eefd3ce022fafe475b
gosho-kazuya <ketsume0211@gmail.com>
2020-04-16 19:03:41 +0900

    fix position problem

--- client/src/components/organisms/Players.tsx
+++ client/src/components/organisms/Players.tsx
@@ -8,13 +8,25 @@ import { Hand } from '../molecules/Hand'
 
 const Container = styled.View``
 
-const PlayerWrap = styled.View<{ relativePosition: number }>`
+interface WithRelativePosition {
+  relativePosition: number
+}
+
+function getTop({ relativePosition }: WithRelativePosition) {
+  if (relativePosition <= 7) {
+    return (7 - relativePosition) * 50
+  } else {
+    return (relativePosition - 8) * 50
+  }
+}
+
+const PlayerWrap = styled.View<WithRelativePosition>`
   position: absolute;
   align-items: center;
-  flex-direction: ${p => (p.relativePosition < 0 ? 'row-reverse' : 'row')};
-  right: ${p => (p.relativePosition < 0 ? 0 : 'unset')};
-  left: ${p => (p.relativePosition > 0 ? 0 : 'unset')};
-  top: ${p => Math.abs(p.relativePosition) * 100}px;
+  flex-direction: ${p => (p.relativePosition <= 7 ? 'row' : 'row-reverse')};
+  right: ${p => (p.relativePosition <= 7 ? 'unset' : 0)};
+  left: ${p => (p.relativePosition <= 7 ? 0 : 'unset')};
+  top: ${getTop}px;
 `
 
 const Betting = styled.View`
@@ -29,17 +41,21 @@ interface PlayersProps {
 }
 
 export function Players({ board, showDown, players, userPosition }: PlayersProps) {
+  const getRelativePosition = React.useCallback(theirPosition => {
+    const allPlayers = players.length + 1 // including me
+    return (
+      ((allPlayers + theirPosition - userPosition) % allPlayers) * Math.floor(1 + 12 / allPlayers)
+    )
+  }, [])
+
   return (
     <Container>
       {players.map(player => (
-        <PlayerWrap
-          key={player.id}
-          relativePosition={(player.position - userPosition) % players.length}
-        >
+        <PlayerWrap key={player.id} relativePosition={getRelativePosition(player.position)}>
           {board.dealerPlayerId === player.id && <DealerButton />}
           <Avatar isTurn={board.turnPlayerId === player.id} player={player} />
-          {/* <div>position: {player.position} , </div> */}
-          {/* <div>relativePosition: {(player.position - userPosition) % players.length}</div> */}
+          <div>position: {player.position}</div>
+          <div>relativePosition: {getRelativePosition(player.position)}</div>
           <Betting>{player.betting > 0 && <Dollar amount={player.betting} />}</Betting>
           {showDown && player.isActive && <Hand cards={player.hand} />}
         </PlayerWrap>
